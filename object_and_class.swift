901c140392bcc28082c55c0787f4f46efaef9142
suyiping <suyiping1@qq.com>
2018-03-21 17:04:27 +0800

    Add files via upload

---
+++ object_and_class.swift
@@ -0,0 +1,124 @@
+class NamedShape {
+    var numberOfSides: Int = 0
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides. \(name)"
+    }
+}
+
+class Circle: NamedShape {
+    var radius: Double
+    
+    init(radius: Double, name: String) {
+        self.radius = radius
+        super.init(name: name)
+    }
+    
+    func area() -> Double {
+        return 3.14 * radius * radius;
+    }
+    
+    override func simpleDescription() -> String {
+        print(super.simpleDescription())
+        return "A circle with radius of length \(radius)."
+    }
+}
+
+var test1 = NamedShape(name: "haha")
+print(test1.simpleDescription())
+
+class Square: NamedShape {
+    var sideLength: Double
+    
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 4
+    }
+    
+    func area() -> Double {
+        return sideLength * sideLength
+    }
+    
+    override func simpleDescription() -> String {
+        return "A square with sides of length \(sideLength)."
+    }
+}
+let test = Square(sideLength: 5.2, name: "my test square")
+print(test.area())
+print(test.simpleDescription())
+
+print("--------------------------------------")
+let circle = Circle(radius: 1.2, name: "haha")
+print(circle.area())
+print(circle.simpleDescription())
+print("--------------------------------------")
+
+class EquilateralTriangle: NamedShape {
+    var sideLength: Double = 0.0
+    
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 3
+    }
+    
+    var perimeter: Double {
+        get {
+            return 3.0 * sideLength
+        }
+        set(newValue) {
+            sideLength = newValue / 3.0
+        }
+    }
+    
+    override func simpleDescription() -> String {
+        return "An equilateral triangle with sides of length \(sideLength)."
+    }
+}
+var triangle = EquilateralTriangle(sideLength: 3.1, name: "a triangle")
+print(triangle.simpleDescription())
+print(triangle.perimeter)
+triangle.perimeter = 9.9
+print(triangle.simpleDescription())
+print(triangle.perimeter)
+print("--------------------------------------")
+
+class TriangleAndSquare {
+    var triangle: EquilateralTriangle {
+        willSet {
+            square.sideLength = newValue.sideLength
+        }
+    }
+    var square: Square {
+        willSet {
+            triangle.sideLength = newValue.sideLength
+        }
+    }
+    init(size: Double, name: String) {
+        square = Square(sideLength: size, name: name)
+        triangle = EquilateralTriangle(sideLength: size, name: name)
+    }
+}
+var triangleAndSquare = TriangleAndSquare(size: 10, name: "another test shape")
+print(triangleAndSquare.square.sideLength)
+print(triangleAndSquare.triangle.sideLength)
+triangleAndSquare.square = Square(sideLength: 50, name: "larger square")
+print(triangleAndSquare.triangle.sideLength)
+
+print("Optinal value--------------------------------------")
+let optionalSquare: Square? = Square(sideLength: 2.5, name: "optional square")
+let sideLength = optionalSquare?.sideLength
+print(String(describing: sideLength))
+print(sideLength ?? "nil")
+print(sideLength!)
+if var tempInt = sideLength {
+    print(tempInt)
+} else {
+    print("sideLength is nil")
+}
